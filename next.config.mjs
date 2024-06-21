d7afa056c03f94eeebf208f164ceb4246a5749c5
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-06-21 12:21:45 -0400

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+// Next.js configuration
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: "export",
+  async generateStaticParams() {
+    return {
+      "/not-found": { page: "/not-found" },
+      "/loading": { page: "/loading" },
+    };
+  },
 };
 
 export default nextConfig;
-
