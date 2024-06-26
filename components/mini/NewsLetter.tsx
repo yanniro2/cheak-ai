c32036b75124f1afc2db67515bde88f4e2c37e1f
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-06-26 12:39:13 -0400

    add function for newsletter

--- components/mini/NewsLetter.tsx
+++ components/mini/NewsLetter.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 type Props = {};
 
 const NewsLetter = (props: Props) => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    fetch("https://formkeep.com/f/cb370d9e61a6", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          setEmail("");
+          alert("Thank you for activate Newsletter");
+        }
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+      });
+  };
+
   return (
     <div className="flex py-2 flex-col gap-3">
       <label htmlFor="nemail" className="text-xl font-bold z-20">
@@ -10,17 +34,19 @@ const NewsLetter = (props: Props) => {
       </label>
       <form
         className="flex items-center"
-        action="https://formkeep.com/f/cb370d9e61a6"
+        onSubmit={handleSubmit}
         accept-charset="UTF-8"
-        encType="multipart/form-data"
-        method="POST">
+        encType="multipart/form-data">
         <input
           placeholder="Email Address"
           className="input-l"
           type="email"
           id="email"
           name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+          required
         />
         <button
           className="border-primary bg-primary py-4 px-6 flex items-center justify-center border rounded-r font-poppins uppercase"
